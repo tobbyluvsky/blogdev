8065ad032b9ec5edec7ca6c1c454f629a92e7d5a
tobbyluvsky <tobby995@gmail.com>
2021-03-26 21:08:09 +0800

    Post detail page done

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -9,10 +9,24 @@ class FrontendController extends Controller
 {
     public function home(){
 
-        $posts = Post::orderBy('created_at','DESC')->take(5)->get();
+
+        //first category posts
+        $posts = Post::with('category','user')->orderBy('created_at','DESC')->take(5)->get();
+        $firstPost2 = $posts->splice(0,2);
+        $middlePost = $posts->splice(0,1);
+        $lastPost = $posts->splice(0);
+
+        //footer category posts
+        $footerPosts = Post::with('category','user')->inRandomOrder()->limit(4)->get();
+        $firstFooterPost = $footerPosts->splice(0,1);
+        $middleFooterPost2 = $footerPosts->splice(0,2);
+        $lastFooterPost = $footerPosts->splice(0,1);
+
+
+        //for general post
         $recentPosts = Post::with('category','user')->orderBy('created_at','DESC')->paginate(10);
 
-        return view('website.index',compact('posts','recentPosts'));
+        return view('website.index',compact('posts','recentPosts','firstPost2','middlePost','lastPost','firstFooterPost','middleFooterPost2','lastFooterPost'));
     }
 
     public function category(){
